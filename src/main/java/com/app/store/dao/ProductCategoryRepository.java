b86735fb5f85d3ccf8cef1e05b3b932b11b0f67a
SoyBrayanRios <contacto.brayanrios@gmail.com>
2022-11-24 23:10:59 -0500

    Initial commit

---
+++ src/main/java/com/app/store/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.app.store.dao;
+
+import com.app.store.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
